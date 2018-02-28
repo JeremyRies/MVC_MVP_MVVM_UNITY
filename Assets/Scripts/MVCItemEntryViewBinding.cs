c352f40a542b1588fadc48eff1576002844f0486
Jeremy <jeremy.ries@fluffyfairygames.com>
2018-02-28 21:17:34 +0100

    cleaned up - made code compile - added more reasonable usage

---
+++ Assets/Scripts/MVCItemEntryViewBinding.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using NUnit.Framework;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Purchasing.Extension;
+using UnityEngine.UI;
+
+public class MVCItemEntryViewBinding : MonoBehaviour
+{
+    public Button BuyButton;
+
+    public Text ItemNameText;
+    public Text ItemCountText;
+}
+
+public class ItemEntryView : MonoBehaviour, IItemEntryView
+{
+    [SerializeField] private MVCItemEntryViewBinding _viewBinding;
+    private ButtonView _buyButtonView;
+
+    public string NameText
+    {
+        set { _viewBinding.ItemNameText.text = value; }
+    }
+
+    public int ItemCount
+    {
+        set { _viewBinding.ItemCountText.text = "Count: "+ value; }
+    }
+
+    public AbstractButtonView ButtonView
+    {
+        get { return _buyButtonView; }
+    }
+
+    public void Initialize(Action action)
+    {
+        _buyButtonView = new ButtonView(action,_viewBinding.BuyButton);
+    }
+}
+
+public interface IItemEntryView
+{
+    string NameText { set; }
+    int ItemCount { set; }
+    AbstractButtonView ButtonView { get; }
+    void Initialize(Action action);
+}
+
+public class ItemEntryViewMock : IItemEntryView
+{
+    private AbstractButtonView _buttonView;
+    public string NameText { get; set; }
+
+    public int ItemCount { get; set; }
+
+    public AbstractButtonView ButtonView
+    {
+        get { return _buttonView; }
+    }
+
+    public void Initialize(Action action)
+    {
+        _buttonView = new MockButtonView(action);
+    }
+}
+
+public class ItemModel : IItemModel
+{
+    private string _itemName = "Life Blob";
+    public ReactiveProperty<int> ItemCount { get; private set; }
+
+    public string ItemName
+    {
+        get { return _itemName; }
+    }
+
+    public void Buy()
+    {
+        ItemCount.Value++;
+    }
+}
+
+public interface IItemModel
+{
+    string ItemName { get; }
+    ReactiveProperty<int> ItemCount { get; }
+    void Buy();
+}
+
+public class ItemEntryController
+{
+    private IItemEntryView _view;
+    private readonly IItemModel _model;
+
+    public ItemEntryController(IItemEntryView view, IItemModel model)
+    {
+        _view = view;
+        _model = model;
+    }
+
+    public void Setup()
+    {
+        _view.Initialize(() => _model.Buy());
+        _view.NameText = _model.ItemName;
+
+        _model.ItemCount.Subscribe(val =>
+        {
+            _view.ItemCount = val;
+            _view.ButtonView.ButtonActive = val < 5;
+        });
+    }
+}
+
---
+++ Assets/Scripts/MVVMCode.cs
@@ -0,0 +1,134 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class MVVMCode
+    {
+        public void EntryPoint()
+        {
+            var model = new BuyItemViewModel(new MockStore(), new MockItemType(), new MockInventory());
+            //normally you would instantiate a prefab here - when mocking you can use a proper constructor though
+            var view = new BuyItemViewMock(model);
+            view.ExcecuteBuy();
+        }
+    }
+
+    public class BuyItemViewMock
+    {
+        private IBuyItemViewModel _viewModel;
+
+        public BuyItemViewMock(IBuyItemViewModel viewModel)
+        {
+            this._viewModel = viewModel;
+        }
+
+        public void ExcecuteBuy()
+        {
+            _viewModel.BuyCommand.Execute();
+        }
+    }
+
+    public class BuyItemView : MonoBehaviour
+    {
+        public Button BuyButton;
+        public Text ItemNameText;
+        public Text ItemCountText;
+
+        public void Initialize(IBuyItemViewModel viewModel)
+        {
+            ItemNameText.text = viewModel.Name;
+            viewModel.BuyCommand.BindTo(BuyButton);
+            viewModel.Amount.Subscribe(val =>
+            {
+                ItemCountText.text = "count: " + val;
+            });
+        }
+    }
+
+    public interface IBuyItemViewModel
+    {
+        string Name { get; }
+        ReactiveProperty<int> Amount { get; }
+        ReactiveCommand BuyCommand { get; }
+    }
+
+    public class BuyItemViewModel : IBuyItemViewModel
+    {
+        private readonly IStore2 _store;
+        private readonly IItemType _item;
+        private readonly IInventory _inventory;
+        private readonly ReactiveCommand _buyCommand;
+
+        public BuyItemViewModel(IStore2 store, IItemType item, IInventory inventory)
+        {
+            _store = store;
+            _item = item;
+            _inventory = inventory;
+            _buyCommand = new ReactiveCommand(_store.CanBuy(item));
+            _buyCommand.Subscribe(_ => _store.Buy(item));
+        }
+
+        public string Name
+        {
+            get { return _item.Name; }
+        }
+
+        public ReactiveProperty<int> Amount
+        {
+            get { return _inventory.AmountOf(_item); }
+        }
+
+        public ReactiveCommand BuyCommand
+        {
+            get { return _buyCommand; }
+        }
+    }
+
+    public interface IStore2
+    {
+        IObservable<bool> CanBuy(IItemType item);
+        void Buy(IItemType item);
+    }
+
+    public interface IInventory
+    {
+        ReactiveProperty<int> AmountOf(IItemType item);
+    }
+
+    public interface IItemType
+    {
+        string Name { get; }
+    }
+
+
+    class MockItemType : IItemType
+    {
+        public string Name
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+    }
+
+    class MockInventory : IInventory
+    {
+        public ReactiveProperty<int> AmountOf(IItemType item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    class MockStore : IStore2
+    {
+        public IObservable<bool> CanBuy(IItemType item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Buy(IItemType item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
